8436976a70fd13c65eb76ad33534a56e8639f7e4
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-06-13 11:02:52 +0530

    Initial Commit

---
+++ MenuFlow/MenuFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuFlowApp.swift
+//  MenuFlow
+//
+//  Created by Shashank Yadav on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
