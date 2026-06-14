51349b3e0e503b36b7601f882b48f875e8a26bb0
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-06-14 16:42:22 +0530

    Creation: layout creation of the Menu

--- MenuFlow/ContentView.swift
+++ MenuFlow/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var menuViewModel = MenuViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack(alignment: .leading,spacing: 12){
+                    ForEach(menuViewModel.menuItems) { item in
+                        HStack(spacing: 15){
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 130,height:140)
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                            
+                            VStack(alignment: .leading){
+                                Text(item.appetizerName)
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                                    .foregroundStyle(.primary)
+                                
+                                Text(menuViewModel.currencyFormatter(number: item.price))
+                                    .font(.headline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                }
+            }
         }
-        .padding()
+       
     }
 }
 
