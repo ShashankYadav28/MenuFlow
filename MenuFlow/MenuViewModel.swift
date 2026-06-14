54d49828fabfb137e2b4fcd9d2e4b21fb80af971
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-06-14 16:40:50 +0530

    Updation: Addition of the Currecy Formatter

--- MenuFlow/MenuViewModel.swift
+++ MenuFlow/MenuViewModel.swift
@@ -17,21 +17,39 @@ class MenuViewModel:ObservableObject {
     
     init () {
 //        service.getMenuItem()
+        getMenuItems()
     }
     
-    func getMenuItem() {
+    func getMenuItems() {
+        
         isLoading = true
         service.getMenuItem()
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
+                case .finished:
+                    print("successFully fteched the Data")
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.isLoading = false
+                    self.errorMessage = error.localizedDescription
                 
                 }
             } receiveValue: { [weak self] menuItems in
+                print("Recived Value: \(menuItems.count)")
+                print(menuItems.first?.appetizerName ?? "No data ")
                 self?.menuItems = menuItems
             }
             .store(in: &cancellables)
 
+    }
+    
+    func currencyFormatter(number:Double)->String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = .current
+        return numberFormatter.string(from: NSNumber(value: number)) ?? "₹0.00"
         
     }
     
